4eaea27d9676a6599207e60515a19c43faf334e4
Rohan Sahai <rohan@sah.ai>
2024-06-02 17:45:13 -0700

    updates

--- wav_to_mp3.py
+++ wav_to_mp3.py
@@ -4,7 +4,7 @@ from pydub import AudioSegment
 # example usage AudioSegment.from_wav("/input/file.wav").export("/output/file.mp3", format="mp3")
 
 # Let's loop through each of the wav files in the folder ./Homemade and convert them to mp3
-for file in os.listdir("./Homemade"):
+for file in os.listdir("./Pressure Slide Masters/24b wav/"):
     if file.endswith(".wav"):
         print(f"Converting {file} to mp3")
-        AudioSegment.from_wav(f"./Homemade/{file}").export(f"./Homemade/{file.replace('.wav', '.mp3')}", format="mp3")
+        AudioSegment.from_wav(f"./Pressure Slide Masters/24b wav/{file}").export(f"./all_mp3s/{file.replace('.wav', '.mp3')}", format="mp3")
